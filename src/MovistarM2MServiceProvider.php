2721d37af7908f1dadcb6e11a0d3958a83ee3263
Ivan berthillood <Ivan berthillood@Ivanberthillood>
2018-10-17 10:23:10 -0300

    Primer Commit

--- src/MovistarM2MServiceProvider.php
+++ src/MovistarM2MServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 
 class MovistarM2MServiceProvider extends ServiceProvider
 {
+    protected $defer = false;
     /**
      * Perform post-registration booting of services.
      *
@@ -13,15 +14,6 @@ class MovistarM2MServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'bionconnection');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'bionconnection');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        // Publishing is only necessary when using the CLI.
-        if ($this->app->runningInConsole()) {
-            $this->bootForConsole();
-        }
     }
 
     /**
@@ -31,15 +23,25 @@ class MovistarM2MServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__.'/../config/movistarm2m.php', 'movistarm2m');
-
-        // Register the service the package provides.
-        $this->app->singleton('movistarm2m', function () {
-            $movistar = new Api(config('movistarm2m.apiKey'));
-            return MovistarM2M($movistar);
-            
-        });
+   
         
+        
+        $this->app->singleton('MovistarM2M', function() {
+
+			return new MovistarM2M();
+
+		});
+
+		$this->app->booting(function() {
+
+		  $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+
+		  $loader->alias('MovistarM2M', 'BionConnection\MovistarM2M\Facades\MovistarM2M');
+
+		});
+		$this->publishes([
+			dirname(__FILE__).'/../config/movistarm2m.php'=>config_path('movistarm2m.php'),
+		]);
         
      
         
@@ -53,37 +55,8 @@ class MovistarM2MServiceProvider extends ServiceProvider
      */
     public function provides()
     {
-        return ['movistarm2m'];
+        return ['MovistarM2M'];
     }
     
-    /**
-     * Console-specific booting.
-     *
-     * @return void
-     */
-    protected function bootForConsole()
-    {
-        // Publishing the configuration file.
-        $this->publishes([
-            __DIR__.'/../config/movistarm2m.php' => config_path('movistarm2m.php'),
-        ], 'movistarm2m.config');
 
-        // Publishing the views.
-        /*$this->publishes([
-            __DIR__.'/../resources/views' => base_path('resources/views/vendor/bionconnection'),
-        ], 'movistarm2m.views');*/
-
-        // Publishing assets.
-        /*$this->publishes([
-            __DIR__.'/../resources/assets' => public_path('vendor/bionconnection'),
-        ], 'movistarm2m.views');*/
-
-        // Publishing the translation files.
-        /*$this->publishes([
-            __DIR__.'/../resources/lang' => resource_path('lang/vendor/bionconnection'),
-        ], 'movistarm2m.views');*/
-
-        // Registering package commands.
-        // $this->commands([]);
-    }
 }
